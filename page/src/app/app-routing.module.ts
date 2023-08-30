af31d59f8df477fbbdb5026c13aa5e43387bfec8
PedroValdovinos <pe.valdovinos@duocuc.cl>
2023-08-29 22:17:11 -0400

    Creacion de pages

--- page/src/app/app-routing.module.ts
+++ page/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'restablecer',
+    loadChildren: () => import('./restablecer/restablecer.module').then( m => m.RestablecerPageModule)
+  },
 ];
 
 @NgModule({
