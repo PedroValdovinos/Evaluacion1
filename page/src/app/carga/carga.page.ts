d8145107ef0c8d6d9b211bbdff2d4da4d02e1e02
PedroValdovinos <pe.valdovinos@duocuc.cl>
2023-11-26 20:49:26 -0300

    page carga

--- page/src/app/carga/carga.page.ts
+++ page/src/app/carga/carga.page.ts
@@ -1,22 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 import { supabase } from '../services/supabase.service';
 import { Time } from '@angular/common';
-
-interface Viaje {
-  id: number;
-  origen: string;
-  destino: string;
-  fecha: Date;
-  hora: Time;
-  costo: Float32Array;
-  asiento_disponible: number;
-  estado: boolean;
-  patente: string;
-  marca: string;
-  modelo: string;
-  color: string;
-}
+import { ActivatedRoute, Params } from '@angular/router';
+import { Viaje } from '../alumno/alumno.page';
 
 @Component({
   selector: 'app-carga',
@@ -24,38 +11,36 @@ interface Viaje {
   styleUrls: ['./carga.page.scss'],
 })
 export class CargaPage implements OnInit {
-  viajeId: number;
-  viaje: Viaje = {} as Viaje;
+  viaje!: Viaje;
 
+  constructor(private route: ActivatedRoute,
+             public navCtrl: NavController,
+            
+             ) {}
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.viajeId = 0;
+             ngOnInit() {
+              // Obtén los datos del viaje de los parámetros de la URL
+              this.route.queryParams.subscribe((params) => {
+                console.log('Params:', params); // Verifica los parámetros en la consola
+                if (params['viaje']) {
+                  this.viaje = JSON.parse(params['viaje']);
+                  console.log('Viaje:', this.viaje);
+                  // Verifica el objeto 'viaje' en la consola
+          
+                  // Comprobar si asientos_disponibles es igual a 0
+                  if (this.viaje.asiento_disponible === 0) {
+                    // Redirigir a la página 'viaje'
+                    this.navCtrl.navigateForward(['/viaje']);
+                  }
+                }
+              });
+            }
+
+      CerrarSesion() {
+    localStorage.removeItem('ingresado');
+    this.navCtrl.navigateRoot('login');
+  
   }
 
-  ngOnInit() {
-    const viajeIdParam = this.activatedRoute.snapshot.paramMap.get('viajeId');
-    if (viajeIdParam !== null) {
-      this.viajeId = +viajeIdParam;
-    } else {
 
-    }
-        this.getViaje();
-  }
-
-  async getViaje() {
-    try {
-      const { data: viajeData, error: viajeError } = await supabase
-        .from('viaje')
-        .select('*')
-        .eq('id', this.viajeId);
-
-      if (viajeError) {
-        console.error('Error al obtener datos del viaje:', viajeError);
-      } else {
-        this.viaje = viajeData[0] as Viaje;
-      }
-    } catch (error) {
-      console.error('Error al obtener datos del viaje:', error);
-    }
-  }
 }
