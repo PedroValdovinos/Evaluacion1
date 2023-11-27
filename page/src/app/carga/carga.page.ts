36352879606b29112cfabdb95c06b12bb00cec98
PedroValdovinos <pe.valdovinos@duocuc.cl>
2023-11-26 22:11:21 -0300

    cambios

--- page/src/app/carga/carga.page.ts
+++ page/src/app/carga/carga.page.ts
@@ -19,17 +19,13 @@ export class CargaPage implements OnInit {
              ) {}
 
              ngOnInit() {
-              // Obtén los datos del viaje de los parámetros de la URL
               this.route.queryParams.subscribe((params) => {
-                console.log('Params:', params); // Verifica los parámetros en la consola
+                console.log('Params:', params);
                 if (params['viaje']) {
                   this.viaje = JSON.parse(params['viaje']);
                   console.log('Viaje:', this.viaje);
-                  // Verifica el objeto 'viaje' en la consola
           
-                  // Comprobar si asientos_disponibles es igual a 0
                   if (this.viaje.asiento_disponible === 0) {
-                    // Redirigir a la página 'viaje'
                     this.navCtrl.navigateForward(['/viaje']);
                   }
                 }
